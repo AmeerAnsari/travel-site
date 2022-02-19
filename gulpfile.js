1755c86305c98dd3b54cf0494078db03ace534ad
AmeerAnsari <hawkthunder111@gmail.com>
2022-02-20 00:31:29 +0500

    Added file sturcture to CSS files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
-// import gulp package
-var gulp = require('gulp'),
-watch = require('gulp-watch');
+var gulp = require('gulp'),    
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+cssvars = require('postcss-simple-vars'),
+autoprefixer = require('autoprefixer'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 
 // default talk
@@ -12,11 +16,31 @@ gulp.task('html', function () {
     console.log("Imagin begin done something useful here.")
 })
 
+// CSS sample task
+gulp.task('styles', function () {
+    // piping the post-processor PostCSS
+    return gulp.src('./app/assets/styles/styles.css')
+        .pipe(postcss([cssImport, cssvars, autoprefixer, nested]))
+        .pipe(gulp.dest('./app/temp/styles'));
+})
+
+
+
+
 
 // gulp watch
-
 gulp.task('watch', function(){
+   
+    // HTML task (index.html)
     watch('./app/index.html', function(){
         gulp.start('html');
     });
-})
+
+    // CSS task (Style.css)
+    watch('./app/assets/styles/**/*.css', function () {
+    gulp.start('styles');
+    });
+
+    // 
+
+});
