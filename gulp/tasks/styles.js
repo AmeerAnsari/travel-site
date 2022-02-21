8ec699ba7a510ac7ba9ca7f6a6a8544099521990
AmeerAnsari <hawkthunder111@gmail.com>
2022-02-21 19:19:00 +0500

    Add mobile first CSS, using PostCSS-mixins

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -2,15 +2,16 @@ var gulp = require('gulp'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
 postcss = require('gulp-postcss'),
-cssImport = require('postcss-import');
 autoprefixer = require('autoprefixer'),
+cssImport = require('postcss-import'),
+mixins = require('postcss-mixins');
 
 
 // CSS sample task
 gulp.task('styles', function () {
     // piping the post-processor PostCSS
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, cssvars, autoprefixer, nested]))
+        .pipe(postcss([cssImport, mixins,cssvars, autoprefixer, nested]))
         .on('error', function(errorInfo){
             console.log(errorInfo.toString());
             this.emit('end');
